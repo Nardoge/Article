58b1f362a5dcc9d6452b4b80b00a231fc3157ec3
nardoge <nardoge@github.com>
2018-09-29 00:32:00 +0800

    Singleton static

---
+++ src/main/java/com/ray/singleton/Singleton.java
@@ -0,0 +1,16 @@
+package com.ray.singleton;
+
+public class Singleton {
+
+    private static  Singleton instance;
+
+    static {
+        instance = new Singleton();
+    }
+
+    private Singleton () {}
+
+    public static Singleton getInstance() {
+        return instance;
+    }
+}
